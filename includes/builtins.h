5e8e6565c73c683620835dcd6cc90e387f19d83a
Thibault COLLARD <tcollard@e2r12p17.42.fr>
2019-03-11 10:53:36 +0100

    start import files

---
+++ includes/builtins.h
@@ -0,0 +1,12 @@
+#ifndef BUILTINS_H
+# define BUILTINS_H
+
+int	cd_builtins(t_ast *elem, t_env **lst_env, t_alloc **alloc);
+int	echo_builtins(t_ast *elem, t_env **lst_env, t_alloc **alloc);
+int	env_builtins(t_ast *elem, t_env **lst_env, t_alloc **alloc);
+int	env_cp(char **env, t_env **lst_env);
+int	exit_builtins(t_ast *elem, t_env **lst_env, t_alloc **alloc);
+int	setenv_builtins(t_ast *elem, t_env **lst_env, t_alloc **alloc);
+int	unsetenv_builtins(t_ast *elem, t_env **lst_env, t_alloc **alloc);
+
+#endif
