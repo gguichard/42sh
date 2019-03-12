9c8d48ade40c8674f088d6bd24e422f2a98f180b
Jonas COHEN <jocohen@e2r11p22.42.fr>
2019-03-12 17:53:18 +0100

    Ajout des operateurs.

---
+++ includes/operator.h
@@ -0,0 +1,58 @@
+#ifndef OPEARTOR_H
+# define OPEARTOR_H
+
+# include "shell.h"
+# include "error.h"
+
+/*
+********************************** OPERATOR ************************************
+*/
+
+int						do_pipe(t_ast *elem, t_var **lst_env, t_alloc **alloc);
+int						job_control(t_ast *elem, t_var *lst_env);
+void					redirection(t_ast *elem, t_var **lst_env,
+						t_alloc **alloc);
+void					heredoc(t_ast *elem, t_var **lst_env, t_alloc **alloc);
+int						complete_heredoc(t_ast *lst, t_alloc **alloc);
+
+/*
+******************************** REDIRECTION ***********************************
+*/
+
+void					redirection_1(t_ast *elem, t_var **lst_env,
+						t_alloc **alloc);
+void					redirection_2(t_ast *elem, t_var **lst_env,
+						t_alloc **alloc);
+void					redirection_3(t_ast *elem, t_var **lst_env,
+						t_alloc **alloc);
+
+/*
+******************************** REDIRECTION ***********************************
+*/
+
+int						agreg_1(t_ast *elem, t_var **lst_env, char **tab_path,
+						t_alloc **alloc);
+int						agreg_2(t_ast *elem, t_var **lst_env, char **tab_path,
+						t_alloc **alloc);
+int						agreg_3(t_ast *elem, t_var **lst_env, char **tab_path,
+						t_alloc **alloc);
+int						agreg_4(t_ast *elem, t_var **lst_env, char **tab_path,
+						t_alloc **alloc);
+int						agreg_5(t_ast *elem, t_var **lst_env, char **tab_path,
+						t_alloc **alloc);
+int						ft_fd_exist(char *str_fd);						
+int						ft_is_redir(t_ast *elem, int *fd, t_alloc *alloc,
+						t_var **lst_env);
+int						ft_is_agreg(t_ast *elem, int *fd, t_alloc *alloc);
+int						heredoc_content(t_alloc *alloc, t_ast *elem, char *s);
+int						get_last_index(int *i, char *s, int *save,
+						char **input);
+int						get_last_operator(int *i, char *s, int *save,
+						char **input);
+int						get_last_digit(int *i, char *s, int *save,
+						char **input);
+void					fill_last_elem(char **s, int i, int save, t_ast *new);
+void					reinit_fd(int *fd, t_alloc *alloc);
+
+
+#endif
---
+++ srcs/operator/pipe.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+#include "operator.h"
+#include "parser_lexer.h"
+
+static int	process_pipe_left(t_ast *elem, t_var **lst_env, t_alloc **alloc, int *fd)
+{
+	int	ret;
+
+	close(fd[0]);
+	dup2(fd[1], STDOUT_FILENO);
+	close(fd[1]);
+	ret = analyzer(elem, lst_env, alloc);
+	exit(ret);
+}
+
+static int	process_pipe_right(t_ast *elem, t_var **lst_env, t_alloc **alloc, int *fd)
+{
+	int	ret;
+
+	close(fd[1]);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
+	ret = analyzer(elem, lst_env, alloc);
+	exit(ret);
+}
+
+int			do_pipe(t_ast *elem, t_var **lst_env, t_alloc **alloc)
+{
+	int	pid1;
+	int	pid2;
+	int	fd[2];
+
+	if (g_pid == -1 || pipe(fd) == -1 || !elem->right || !elem->left)
+		return (0);
+	if (!(pid1 = fork()))
+		process_pipe_left(elem->left, lst_env, alloc, fd);
+	else
+	{
+		g_pid = pid1;
+		if (!(pid2 = fork()))
+			process_pipe_right(elem->right, lst_env, alloc, fd);
+		else
+		{
+			g_pid = pid2;
+			close(fd[1]);
+			close(fd[0]);
+			waitpid(pid1, &(g_ret[0]), 0);
+			waitpid(pid2, &(g_ret[0]), 0);
+			g_ret[1] = 1;
+		}
+	}
+	return (1);
+}
