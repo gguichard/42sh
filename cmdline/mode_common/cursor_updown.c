4328ed9a80530a190980b4d12f47534f3ec0f72c
Gwennael Guichard <gguichar@student.42.fr>
2019-03-14 00:51:24 +0100

    Fix cursor down, TODO: vertical up

--- cmdline/mode_common/cursor_updown.c
+++ cmdline/mode_common/cursor_updown.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 12:39:11 by gguichar          #+#    #+#             */
-/*   Updated: 2019/03/13 22:23:38 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/03/14 00:49:30 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,30 @@
 
 int	handle_cursor_up(t_cmdline *cmdline)
 {
-	int	idx;
-	int	row;
+	int	offset;
 	int	rightest_col;
 	int	col;
 
 	if (cmdline->saved_col == -1)
 		cmdline->saved_col = cmdline->cursor.x;
-	if (cmdline->row == 0
-			|| (idx = cmdline->input.offset - cmdline->cursor.x) <= 0)
+	if (cmdline->row == 0)
 		return (0);
-	else
+	offset = cmdline->input.offset - cmdline->cursor.x;
+	rightest_col = (cmdline->row - 1) == 0 ? (cmdline->prompt.offset + 1) : 1;
+	while (offset > 0
+			&& rightest_col < cmdline->winsize.ws_col
+			&& cmdline->input.buffer[offset] != '\n')
 	{
-		rightest_col = 1;
-		if (cmdline->row == 1)
-			rightest_col += cmdline->prompt.offset;
-		while (idx > 0 && rightest_col < cmdline->winsize.ws_col
-				&& cmdline->input.buffer[idx] != '\n')
-		{
-			idx--;
-			rightest_col++;
-		}
-		col = cmdline->saved_col;
-		if (col > rightest_col)
-			col = rightest_col;
-		else if (cmdline->row == 1 && col <= cmdline->prompt.offset)
-			col = cmdline->prompt.offset + 1;
-		idx = cmdline->input.offset - cmdline->cursor.x - (rightest_col - col);
+		offset--;
+		rightest_col++;
 	}
-	cmdline->input.offset = idx;
+	col = 1;
+	while (col < rightest_col && col < cmdline->saved_col)
+	{
+		col++;
+		offset++;
+	}
+	cmdline->input.offset = offset;
 	cmdline->row -= 1;
 	cmdline->cursor.x = col;
 	cmdline->cursor.y -= 1;
@@ -53,30 +48,33 @@ int	handle_cursor_up(t_cmdline *cmdline)
 
 int	handle_cursor_down(t_cmdline *cmdline)
 {
-	int	idx;
 	int	row;
 	int	col;
+	int	offset;
 
 	if (cmdline->saved_col == -1)
 		cmdline->saved_col = cmdline->cursor.x;
-	idx = cmdline->input.offset;
 	row = cmdline->row;
 	col = cmdline->cursor.x;
-	while (idx < cmdline->input.size
+	offset = cmdline->input.offset;
+	while (offset < cmdline->input.size
 			&& (row != cmdline->row + 1 || col < cmdline->saved_col))
 	{
-		if (cmdline->input.buffer[idx] != '\n' && col < cmdline->winsize.ws_col)
+		if (cmdline->input.buffer[offset] != '\n'
+				&& col < cmdline->winsize.ws_col)
 			col++;
 		else
 		{
+			if (row != cmdline->row)
+				break ;
 			col = 1;
 			row++;
 		}
-		idx++;
+		offset++;
 	}
 	if (row == cmdline->row)
 		return (0);
-	cmdline->input.offset = idx;
+	cmdline->input.offset = offset;
 	cmdline->row += 1;
 	cmdline->cursor.x = col;
 	cmdline->cursor.y += 1;
