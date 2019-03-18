2de35b26568316ccd24af76010a7009a6026cd66
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-03-18 13:16:45 +0100

    Remplacement int par wint_t

--- cmdline/mode_insert/del_hooks.c
+++ cmdline/mode_insert/del_hooks.c
@@ -6,21 +6,22 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 16:26:49 by gguichar          #+#    #+#             */
-/*   Updated: 2019/03/18 00:30:33 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/03/18 13:16:21 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include "cmdline.h"
 #include "libft.h"
+#include <unistd.h>
+#include <wchar.h>
 #include <term.h>
+#include "cmdline.h"
 
 static void	del_char_on_cursor(t_cmdline *cmdline)
 {
 	ft_memcpy(cmdline->input.buffer + cmdline->input.offset
 			, cmdline->input.buffer + cmdline->input.offset + 1
 			, (cmdline->input.size - cmdline->input.offset + 1)
-			* sizeof(int));
+			* sizeof(wint_t));
 	cmdline->input.size -= 1;
 	clear_after_cursor(cmdline->cursor, cmdline->winsize);
 	update_cmdline_after_offset(cmdline);
