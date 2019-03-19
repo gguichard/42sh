d34a7e556f84ae38db4555e26772e1e151cfb6ac
tcollard <tcollard@student.42.fr>
2019-03-19 16:26:26 +0100

    update builtin type: add alias

--- srcs/builtins/type.c
+++ srcs/builtins/type.c
@@ -2,10 +2,8 @@
 #include "builtins.h"
 #include "exectable.h"
 #include "hashtable.h"
+#include "aliastable.h"
 #include "search_exec.h"
-
-// BUILTIN TYPE
-// ALIAS SEARCH TYPE NOT SET
 
 int			type_builtins(t_ast *elem, t_alloc *alloc)
 {
@@ -17,13 +15,18 @@ int			type_builtins(t_ast *elem, t_alloc *alloc)
 	i = 0;
 	ret = 0;
 	while (elem->input[++i])
-		if ((path = (char *)get_exec_path(alloc->exectable, elem->input[i], 0)))
+		if ((path = (char *)get_alias(alloc->aliastable, elem->input[i])))
+			ret = ft_printf("%s is aliased to `%s`\n", elem->input[i], path);
+		else if ((path = (char *)get_exec_path(alloc->exectable,
+			elem->input[i], 0)))
 			ret = ft_printf("%s is hashed (%s)\n", elem->input[i], path);
 		else if (is_builtins(alloc, elem->input[i]) == 1)
 			ret = ft_printf("%s is a shell builtin\n", elem->input[i]);
-		else if (ft_strchr(elem->input[i], '/') && access(elem->input[i], F_OK) == 0 && access(elem->input[i], X_OK) == 0)
+		else if (ft_strchr(elem->input[i], '/') && access(elem->input[i], F_OK)
+		== 0 && access(elem->input[i], X_OK) == 0)
 				ret = ft_printf("%s is %s\n", elem->input[i], elem->input[i]);
-		else if (!(ft_strchr(elem->input[i], '/')) && (path = search_exec(*(alloc->var), elem->input[i], &error)))
+		else if (!(ft_strchr(elem->input[i], '/')) && (path =
+			search_exec(*(alloc->var), elem->input[i], &error)))
 		{
 			ret = ft_printf("%s is %s\n", elem->input[i], path);
 			ft_strdel(&path);
@@ -35,7 +38,3 @@ int			type_builtins(t_ast *elem, t_alloc *alloc)
 		}
 	return ((ret != 1 && ret != -1) ? 0 : 1);
 }
-
-// POUT T_ERROR QUI VA DANS SEARCH_EXEC
-// JE SAIS PAS SI IL FAUT LE SETTER DANS LA FONCTION / LE RECUPERER D'AVANT
-// OU LE RETOURNER
