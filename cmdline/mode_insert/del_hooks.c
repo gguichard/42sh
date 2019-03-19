d3b117120b9cf48338ab68b64a8349368e79400f
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-03-19 12:10:29 +0100

    Fix invalid read 2

--- cmdline/mode_insert/del_hooks.c
+++ cmdline/mode_insert/del_hooks.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 16:26:49 by gguichar          #+#    #+#             */
-/*   Updated: 2019/03/18 13:16:21 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/03/19 12:09:39 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static void	del_char_on_cursor(t_cmdline *cmdline)
 {
 	ft_memcpy(cmdline->input.buffer + cmdline->input.offset
 			, cmdline->input.buffer + cmdline->input.offset + 1
-			, (cmdline->input.size - cmdline->input.offset + 1)
+			, (cmdline->input.size - cmdline->input.offset)
 			* sizeof(wint_t));
 	cmdline->input.size -= 1;
 	clear_after_cursor(cmdline->cursor, cmdline->winsize);
