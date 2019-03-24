1609a246d08d1aadaf4534ae04e3893be20e3747
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-03-24 15:39:56 +0100

    Modif deplacement mot par mot

--- srcs/cmdline/mode_common/cursor_words.c
+++ srcs/cmdline/mode_common/cursor_words.c
@@ -6,10 +6,11 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 19:56:15 by gguichar          #+#    #+#             */
-/*   Updated: 2019/03/13 19:52:05 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/03/24 15:38:54 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "cmdline.h"
 
 int	handle_prev_word(t_cmdline *cmdline)
@@ -18,13 +19,11 @@ int	handle_prev_word(t_cmdline *cmdline)
 	if (cmdline->input.offset == 0)
 		return (0);
 	while (cmdline->input.offset > 0
-			&& (cmdline->input.buffer[cmdline->input.offset - 1] == ' '
-				|| cmdline->input.buffer[cmdline->input.offset - 1] == '\n')
+			&& ft_isspace(cmdline->input.buffer[cmdline->input.offset - 1])
 			&& handle_move_left(cmdline))
 		continue ;
 	while (cmdline->input.offset > 0
-			&& cmdline->input.buffer[cmdline->input.offset - 1] != ' '
-			&& cmdline->input.buffer[cmdline->input.offset - 1] != '\n'
+			&& !ft_isspace(cmdline->input.buffer[cmdline->input.offset - 1])
 			&& handle_move_left(cmdline))
 		continue ;
 	return (1);
@@ -36,13 +35,11 @@ int	handle_next_word(t_cmdline *cmdline)
 	if (cmdline->input.offset == cmdline->input.size)
 		return (0);
 	while (cmdline->input.offset < cmdline->input.size
-			&& cmdline->input.buffer[cmdline->input.offset] != ' '
-			&& cmdline->input.buffer[cmdline->input.offset] != '\n'
+			&& !ft_isspace(cmdline->input.buffer[cmdline->input.offset])
 			&& handle_move_right(cmdline))
 		continue ;
 	while (cmdline->input.offset < cmdline->input.size
-			&& (cmdline->input.buffer[cmdline->input.offset] == ' '
-				|| cmdline->input.buffer[cmdline->input.offset] == '\n')
+			&& ft_isspace(cmdline->input.buffer[cmdline->input.offset])
 			&& handle_move_right(cmdline))
 		continue ;
 	return (1);
