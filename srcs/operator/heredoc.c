07056f3e68aeaa7dd16ef589265303969da9cc4e
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-03-27 15:42:24 +0100

    Creation de la fonction pour prompt un heredoc

--- srcs/operator/heredoc.c
+++ srcs/operator/heredoc.c
@@ -1,76 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/27 14:51:18 by gguichar          #+#    #+#             */
+/*   Updated: 2019/03/27 15:42:06 by gguichar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
 #include "shell.h"
-#include "parser_lexer.h"
-#include "operator.h"
+#include "cmdline.h"
 
-// static int		pipe_exec(t_ast *elem, t_alloc *alloc, int *fd)
-// {
-// 	int		ret;
-// 	t_ast	*tmp;
-// 	int		opt;
-//
-// 	tmp = elem;
-// 	while (!ft_strcmp("<<", tmp->input[0]))
-// 		tmp = tmp->left;
-// 	if (!tmp)
-// 		exit(1);
-// 	opt = do_not_fork(tmp, alloc);
-// 	close(fd[1]);
-// 	dup2(fd[0], STDIN_FILENO);
-// 	close(fd[0]);
-// 	ret = analyzer(tmp, alloc, opt);
-// 	exit(ret);
-// }
-//
-// static void		write_pipe(t_ast *elem, int *fd)
-// {
-// 	close(fd[0]);
-// 	dup2(fd[1], STDOUT_FILENO);
-// 	close(fd[1]);
-// 	write(STDOUT_FILENO, elem->heredoc, ft_strlen(elem->heredoc));
-// 	exit(0);
-// }
-//
-// int				complete_heredoc(t_ast *lst, t_alloc *alloc)
-// {
-// 	while (lst)
-// 	{
-// 		if (lst->type == HEREDOC)
-// 		{
-// 			if (!heredoc_content(alloc, lst, 0))
-// 				return (0);
-// 		}
-// 		lst = lst->next;
-// 	}
-// 	return (1);
-// }
+static char	*join_heredoc(char *heredoc, char *part)
+{
+	char	*tab[3];
+	char	*ret;
 
-int				heredoc(t_ast *elem, t_alloc *alloc)
+	tab[0] = heredoc;
+	tab[1] = part;
+	tab[2] = NULL;
+	ret = ft_join(tab, "\n");
+	free(heredoc);
+	return (ret);
+}
+
+char		*read_heredoc(t_cmdline *cmdline, const char *word)
+{
+	char		*heredoc;
+	t_rstate	state;
+
+	heredoc = ft_strdup("");
+	if (heredoc == NULL)
+		return (NULL);
+	setup_term(cmdline);
+	while (1)
+	{
+		state = read_input(cmdline, get_prompt(cmdline, PROMPT_HEREDOC));
+		if (state != RSTATE_END)
+			break ;
+		if (ft_strequ(cmdline->input.buffer, word))
+			break ;
+		heredoc = join_heredoc(heredoc, cmdline->input.buffer);
+		if (heredoc == NULL)
+			break ;
+	}
+	reset_term(cmdline);
+	return (heredoc);
+}
+
+int			heredoc(t_ast *elem, t_alloc *alloc)
 {
 	(void)elem;
 	(void)alloc;
 	return (0);
-	// int		pid1;
-	// int		fd[2];
-	// int		pid2;
-	//
-	// if (!elem->heredoc || !elem->left || pipe(fd) == -1 || (pid1 = fork()) == -1)
-	// 	return (1);
-	// else if (!pid1)
-	// 	write_pipe(elem, fd);
-	// else
-	// {
-	// 	if ((pid2 = fork()) == -1)
-	// 		return (1);
-	// 	else if (!pid2)
-	// 		pipe_exec(elem->left, alloc, fd);
-	// 	else
-	// 	{
-	// 		close(fd[1]);
-	// 		close(fd[0]);
-	// 		waitpid(pid1, NULL, 0);
-	// 		waitpid(pid2, &alloc->ret_val, 0);
-	// 	}
-	// }
-	// ft_memdel((void **)&elem->heredoc);
-	// return (ret_status(alloc->ret_val, 0));
 }
