c5249f27f2589d301bf9a486da7d5d511f822ba2
tcollard <tcollard@student.42.fr>
2019-03-29 16:31:05 +0100

    Add inhib in analyzer + remove opt variable pour autocompletion

--- includes/inhibitor.h
+++ includes/inhibitor.h
@@ -6,7 +6,7 @@
 
 int		inhib_in_db(t_str_cmd_inf *str_cmd, size_t *pos, char **input
 	, t_alloc *alloc);
-char	**inhib_expand_str(const char *str, t_alloc *alloc, int opt);
+char	**inhib_expand_str(const char *str, t_alloc *alloc);
 int		inhib_expand_tab(t_ast *elem, t_alloc *alloc);
 int		inhib_expand_in_quote(t_str_cmd_inf *str_cmd, char **array
 		, size_t *pos_array, t_alloc *alloc);
@@ -17,12 +17,8 @@ int		inhib_expand_in_quote(t_str_cmd_inf *str_cmd, char **array
 
 size_t	get_pos_in_array(char **array);
 void	remove_escaped_char(t_str_cmd_inf *str_cmd, char **input, size_t *pos);
-void	remove_escaped_char_autocomplete(t_str_cmd_inf *str_cmd, char **input
-		, size_t *pos);
 int		go_to_end_quote(t_str_cmd_inf *str_cmd, char **input, size_t *pos);
 void	update_pos_index(t_str_cmd_inf *str_cmd);
-void	remove_escaped_char_select(t_str_cmd_inf *str_cmd, char **input
-		, size_t *pos_array, int opt);
 char	**insert_new_tab(char **modify, int *i, char **new, t_ast *elem);
 void	create_new_input(t_ast *elem, int *i, char **new);
 char	**error_inhib_expand(t_str_cmd_inf *str_cmd, char **array);
