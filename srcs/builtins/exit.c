4587cc91a6cd780fa4eeeb5365c7c3740d67d5f2
Gwennael Guichard <gguichar@student.42.fr>
2019-03-30 12:52:58 +0100

    Suppression includes inutiles / modif pour l'autocompletion

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -76,6 +76,7 @@ int			builtin_exit(t_ast *elem, t_alloc *alloc)
 	ft_putstr("exit\n");
 	terminate_all_jobs();
 	save_history_entries(alloc, &alloc->cmdline.history);
+	// TODO: clean history
 	reset_term(&alloc->cmdline);
 	del_alloc(alloc);
 	exit(status);
--- srcs/main.c
+++ srcs/main.c
@@ -1,97 +1,61 @@
+#include <unistd.h>
+#include <stdlib.h>
 #include "libft.h"
 #include "shell.h"
 #include "cmdline.h"
-#include "builtins.h"
 #include "execution.h"
-#include "exectable.h"
-#include "hashtable.h"
 #include "parser_lexer.h"
 #include "str_cmd_inf.h"
 #include "split_cmd_token.h"
-#include "token_inf.h"
+#include "builtins.h"
 #include "job.h"
-#include "error.h"
 
 static void	lexer_parser(char *line, t_alloc *alloc)
 {
-	t_ast			*sort_ast;
-	t_list			*lst_tk;
-	t_exec_opt		exec_option;
 	t_str_cmd_inf	scmd;
+	t_list			*lst_tk;
+	t_ast			*sort_ast;
+	t_exec_opt		exec_opt;
 
-	sort_ast = NULL;
-	lst_tk = NULL;
-	ft_bzero(&exec_option, sizeof(t_exec_opt));
 	if (!scmd_init(&scmd, line))
 		return ;
 	lst_tk = split_cmd_token(&scmd, alloc->aliastable);
 	scmd_clean(&scmd);
-
-	/*
-	 **	VERIF TOKEN AND PRINT BEFORE PARSE
-	 */
-	// t_list	*tmp;
-	// tmp = lst_tk;
-	// while (tmp)
-	// {
-	// 	ft_printf("type: %d\ntoken: |%s|\n\n", get_tk(tmp)->type, get_tk(tmp)->token);
-	// 	tmp = tmp->next;
-	// }
-
 	sort_ast = parser(lst_tk);
 	ft_lstdel(&lst_tk, del_token);
-
-	/*
-	 ** COMPARAISON POUR RECONNAITRE LE JOB CONTROL
-	 */
-	// if (get_tk(lst_tk)->type == TK_CMD_SEP
-	// && ft_strcmp(get_tk(lst_tk)->token, "&") == 0)
-	// 	analyzer(sort_ast, alloc, TRUE);
-	// else
-
-	/*
-	 ** PRINT AST AND REINIT NODE
-	 */
-	 if (sort_ast)
-		read_sort_descent(sort_ast, 0);
-
+	//if (sort_ast)
+	//	read_sort_descent(sort_ast, 0);
 	check_exit_cmd(sort_ast);
-	alloc->ret_val = analyzer(alloc, sort_ast, &exec_option);
-
-	//FUNCTION TO CLEAN / CLEAN TK_LIST MISSING
+	ft_memset(&exec_opt, 0, sizeof(t_exec_opt));
+	alloc->ret_val = analyzer(alloc, sort_ast, &exec_opt);
 	del_ast(&sort_ast);
 }
 
 int		main(int argc, char **argv, char **environ)
 {
 	t_alloc	alloc;
-	char	*input;
 
-	g_jobs = 0;
+	g_jobs = NULL;
 	if (!setup_alloc(&alloc, argc, argv, environ))
-		ft_dprintf(STDERR_FILENO, "Unable to setup environment\n");
+		ft_dprintf(STDERR_FILENO, "42sh: unable to setup environment\n");
+	else if (!init_cmdline(&alloc, &alloc.cmdline))
+		ft_dprintf(STDERR_FILENO, "42sh: unable to init term\n");
 	else
 	{
-		if (!init_cmdline(&alloc, &alloc.cmdline))
-			ft_dprintf(STDERR_FILENO, "Unable to init term\n");
-		else
+		load_history_file_entries(&alloc, &alloc.cmdline.history);
+		while (1)
 		{
-			load_history_file_entries(&alloc, &alloc.cmdline.history);
-			while (1)
+			refresh_jobs();
+			setup_term(&alloc.cmdline);
+			read_cmdline(&alloc, &alloc.cmdline);
+			reset_term(&alloc.cmdline);
+			if (alloc.full_input != NULL)
 			{
-				refresh_jobs();
-				setup_term(&alloc.cmdline);
-				input = read_cmdline(&alloc, &alloc.cmdline);
-				reset_term(&alloc.cmdline);
-				if (input != NULL)
-				{
-					lexer_parser(input, &alloc);
-					free(input);
-				}
+				lexer_parser(alloc.full_input, &alloc);
+				ft_strdel(&alloc.full_input);
 			}
 		}
 	}
-	terminate_all_jobs();
 	del_alloc(&alloc);
 	return (1);
 }
