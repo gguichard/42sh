40342a384f9eff0e20851b55e3dc67585a983751
Gwennael Guichard <gguichar@student.42.fr>
2019-03-31 18:41:30 +0200

    Fix simple quote

--- srcs/cmdline/history_expand.c
+++ srcs/cmdline/history_expand.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/26 11:13:19 by gguichar          #+#    #+#             */
-/*   Updated: 2019/03/31 16:33:31 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/03/31 16:40:14 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,8 @@ int			expand_history_events(t_history *history, char **input)
 		if ((*exp_inf.input)[exp_inf.offset] == '\"' && !exp_inf.is_in_quote)
 			exp_inf.is_in_dbquote = !exp_inf.is_in_dbquote;
 		else if ((*exp_inf.input)[exp_inf.offset] == '\''
-				&& !exp_inf.is_in_dbquote && !is_char_escaped(&exp_inf))
+				&& !exp_inf.is_in_dbquote
+				&& (exp_inf.is_in_quote || !is_char_escaped(&exp_inf)))
 			exp_inf.is_in_quote = !exp_inf.is_in_quote;
 		else if ((*exp_inf.input)[exp_inf.offset] == '!'
 				&& !exp_inf.is_in_quote && !is_char_escaped(&exp_inf))
