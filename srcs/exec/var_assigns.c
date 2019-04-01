ac72f17ae1348a1e31a3774eaca1988308e34262
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-04-01 14:25:36 +0200

    Assignation de variables temporaires

--- srcs/exec/var_assigns.c
+++ srcs/exec/var_assigns.c
@@ -1,15 +1,21 @@
 #include <stdlib.h>
 #include "shell.h"
+#include "execution.h"
 #include "vars.h"
 
 static void	update_var_or_create_local(t_list **vars, const char *key
-		, const char *value)
+		, const char *value, int is_tmp)
 {
 	t_var	*var;
 
 	var = get_var(*vars, key);
 	if (var == NULL)
-		create_var(vars, key, value, 0);
+		create_var(vars, key, value, is_tmp ? 2 : 0);
+	else if (is_tmp)
+	{
+		free(var->tmp_value);
+		var->tmp_value = ft_strdup(value);
+	}
 	else
 	{
 		free(var->value);
@@ -17,7 +23,7 @@ static void	update_var_or_create_local(t_list **vars, const char *key
 	}
 }
 
-static void	assign_local_vars(t_alloc *alloc, t_ast *elem)
+static void	assign_local_vars(t_alloc *alloc, t_ast *elem, int is_tmp)
 {
 	int		idx;
 	char	*tmp;
@@ -29,15 +35,53 @@ static void	assign_local_vars(t_alloc *alloc, t_ast *elem)
 		if (tmp != NULL)
 		{
 			*tmp = '\0';
-			update_var_or_create_local(&alloc->vars, elem->input[idx], tmp + 1);
+			update_var_or_create_local(&alloc->vars, elem->input[idx], tmp + 1
+					, is_tmp);
 			*tmp = '=';
 		}
 		idx++;
 	}
 }
 
-void		process_assigns(t_alloc *alloc, t_ast *elem)
+static void	del_tmp_vars(t_alloc *alloc)
 {
+	t_list	*cur;
+	t_list	*prev;
+	t_list	*next;
+
+	cur = alloc->vars;
+	prev = NULL;
+	while (cur != NULL)
+	{
+		next = cur->next;
+		if (((t_var *)cur->content)->is_env != 2
+				&& ((t_var *)cur->content)->tmp_value == NULL)
+			prev = cur;
+		else if (((t_var *)cur->content)->is_env != 2)
+			ft_strdel(&((t_var *)cur->content)->tmp_value);
+		else
+		{
+			ft_lstdelone(&cur, free_var);
+			if (prev == NULL)
+				alloc->vars = next;
+			else
+				prev->next = next;
+		}
+		cur = next;
+	}
+}
+
+int			process_assigns(t_alloc *alloc, t_ast *elem, t_exec_opt *opt)
+{
+	int	ret;
+
 	if (elem->left == NULL)
-		assign_local_vars(alloc, elem);
+	{
+		assign_local_vars(alloc, elem, 0);
+		return (analyzer(alloc, elem->left, opt));
+	}
+	assign_local_vars(alloc, elem, 1);
+	ret = analyzer(alloc, elem->left, opt);
+	del_tmp_vars(alloc);
+	return (ret);
 }
