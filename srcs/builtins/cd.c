503e56e9dff82a8c2a570c1f5a8c36c29b9120eb
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-04-04 12:09:27 +0200

    Modif builtin cd

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,11 +6,10 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/25 20:20:40 by gguichar          #+#    #+#             */
-/*   Updated: 2019/04/02 19:34:25 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/04/04 12:07:58 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <pwd.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -22,88 +21,29 @@
 #include "check_path.h"
 #include "builtins.h"
 
-static char	*get_path_or_cwd(t_alloc *alloc, const char *name)
+static char			*get_path_or_cwd(t_alloc *alloc, const char *name)
 {
-	t_var			*var;
-	const char		*login;
+	t_var	*var;
 
 	var = get_var(alloc->vars, name);
 	if (var == NULL)
-	{
-		if (ft_strequ(name, "HOME") && (login = getlogin()) != NULL)
-			return (ft_strdup(getpwnam(login)->pw_dir));
 		return (getcwd(NULL, PATH_MAX));
-	}
-	return (ft_strdup(var->value));
-}
-
-static char	*get_new_cur_path(t_alloc *alloc, const char *cur_path)
-{
-	char	*pwd;
-	char	*new_path;
-
-	if (cur_path[0] == '/')
-		new_path = ft_strdup(cur_path);
 	else
-	{
-		new_path = NULL;
-		pwd = get_path_or_cwd(alloc, "PWD");
-		if (!ft_strequ(cur_path, ".")
-				&& !ft_strequ(cur_path, "..")
-				&& !ft_strnequ(cur_path, "./", 2)
-				&& !ft_strnequ(cur_path, "../", 3))
-			new_path = search_in_cd_path(alloc, cur_path, pwd);
-		if (new_path != NULL)
-			ft_printf("%s\n", new_path);
-		else
-		{
-			new_path = join_path(pwd, cur_path);
-			free(pwd);
-		}
-	}
-	return (new_path);
+		return (ft_strdup(var->value));
 }
 
-static char	*get_cur_path(t_alloc *alloc, t_opts *opts, const char *operand)
-{
-	char	*cur_path;
-	char	*tmp;
-
-	cur_path = NULL;
-	if (operand == NULL)
-		cur_path = get_path_or_cwd(alloc, "HOME");
-	else if (ft_strequ(operand, "-"))
-		cur_path = get_path_or_cwd(alloc, "OLDPWD");
-	else
-		cur_path = ft_strdup(operand);
-	if (cur_path != NULL)
-	{
-		tmp = get_new_cur_path(alloc, cur_path);
-		free(cur_path);
-		cur_path = tmp;
-		if (cur_path != NULL && !has_opt(opts, 'P'))
-			set_dir_to_canonical_form(cur_path);
-	}
-	return (cur_path);
-}
-
-static int	change_dir(t_alloc *alloc, t_opts *opts, const char *cur_path
-		, const char *operand)
+static int			change_dir(t_alloc *alloc, t_opts *opts
+		, const char *cur_path, const char *operand)
 {
 	char		*pwd;
 	t_error		error;
-	const char	*def_path;
 	char		*freed_path;
 	const char	*new_pwd_path;
 
 	if (chdir(cur_path) == -1)
 	{
 		error = check_dir_rights(cur_path, X_OK);
-		def_path = operand;
-		if (def_path == NULL || ft_strequ(def_path, "-"))
-			def_path = cur_path;
-		ft_dprintf(STDERR_FILENO, "cd: %s: %s\n", def_path
-				, error_to_str(error));
+		ft_dprintf(STDERR_FILENO, "cd: %s: %s\n", operand, error_to_str(error));
 		return (1);
 	}
 	pwd = get_path_or_cwd(alloc, "PWD");
@@ -117,28 +57,74 @@ static int	change_dir(t_alloc *alloc, t_opts *opts, const char *cur_path
 	return (0);
 }
 
-int			builtin_cd(t_ast *elem, t_alloc *alloc)
+static char			*get_cur_path(t_alloc *alloc, t_opts *opts
+		, const char *operand)
 {
-	t_opts	opts;
-	int		ret;
 	char	*cur_path;
+	char	*pwd;
 
-	parse_opts(&opts, elem->input, "{LP}");
-	if (opts.error != 0)
+	if (operand[0] == '/')
+		cur_path = ft_strdup(operand);
+	else
+	{
+		cur_path = NULL;
+		pwd = get_path_or_cwd(alloc, "PWD");
+		if (!ft_strequ(operand, ".")
+				&& !ft_strequ(operand, "..")
+				&& !ft_strnequ(operand, "./", 2)
+				&& !ft_strnequ(operand, "../", 3))
+			cur_path = search_in_cd_path(alloc, operand, pwd);
+		if (cur_path != NULL)
+			ft_printf("%s\n", cur_path);
+		else
+			cur_path = join_path(pwd, operand);
+		free(pwd);
+	}
+	if (cur_path != NULL && !has_opt(opts, 'P'))
+		set_dir_to_canonical_form(cur_path);
+	return (cur_path);
+}
+
+static const char	*get_operand_with_replacement(t_alloc *alloc
+		, const char *operand)
+{
+	if (operand == NULL)
+	{
+		operand = get_var_value_or_null(alloc->vars, "HOME");
+		if (operand == NULL)
+			ft_dprintf(STDERR_FILENO, "42sh: cd: HOME not set\n");
+	}
+	else if (ft_strequ(operand, "-"))
+	{
+		operand = get_var_value_or_null(alloc->vars, "OLDPWD");
+		if (operand == NULL)
+			ft_dprintf(STDERR_FILENO, "42sh: cd: OLDPWD not set\n");
+	}
+	return (operand);
+}
+
+int					builtin_cd(t_ast *elem, t_alloc *alloc)
+{
+	t_opts		opts;
+	const char	*operand;
+	char		*cur_path;
+	int			ret;
+
+	if (parse_opts(&opts, elem->input, "{LP}")->error != 0)
 	{
 		ft_dprintf(STDERR_FILENO, "42sh: cd: -%c: invalid option\n"
 				"cd: usage: cd [-L|-P] [dir]\n", opts.error);
 		return (2);
 	}
-	cur_path = get_cur_path(alloc, &opts, elem->input[opts.index]);
-	if (cur_path == NULL)
-	{
-		ret = 1;
+	operand = get_operand_with_replacement(alloc, elem->input[opts.index]);
+	if (operand == NULL)
+		return (1);
+	cur_path = get_cur_path(alloc, &opts, operand);
+	if (cur_path == NULL && (ret = 1))
 		ft_dprintf(STDERR_FILENO, "42sh: cd: unexpected error\n");
-	}
 	else
 	{
-		ret = change_dir(alloc, &opts, cur_path, elem->input[opts.index]);
+		ret = change_dir(alloc, &opts, cur_path, operand);
 		if (ret == 0 && ft_strequ(elem->input[opts.index], "-"))
 			ft_printf("%s\n", cur_path);
 		free(cur_path);
