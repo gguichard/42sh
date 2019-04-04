32787703e6e74248bd35b9a0e89c1c8fdf17bffb
Franck W <franck.rj.w@gmail.com>
2019-04-04 15:42:10 +0200

    Ajout de tests basiques pour les alias.

--- srcs/test_main.c
+++ srcs/test_main.c
@@ -49,22 +49,22 @@ void	test_this_autocomplete(char *strbase, char *result_needed
 	t_str_cmd_inf scmd;
 	t_ac_suff_inf *acs;
 
-	printf("Test ac « %s » :\n", strbase);
+	ft_printf("Test ac « %s » :\n", strbase);
 	scmd_init(&scmd, strbase);
 	acs = autocomplete_cmdline(&scmd, &alloc);
-	printf("(str) Attendu : « %s », resultat : « %s »\n", result_needed, acs->suff);
-	printf("(typ) Attendu : « %d », resultat : « %d »\n", type_needed, acs->suff_type);
+	ft_printf("(str) Attendu : « %s », resultat : « %s »\n", result_needed, acs->suff);
+	ft_printf("(typ) Attendu : « %d », resultat : « %d »\n", type_needed, acs->suff_type);
 	if (ft_strequ(result_needed, acs->suff) && type_needed == acs->suff_type)
-		printf("\033[1;32mOK !\033[0m\n");
+		ft_printf("\033[1;32mOK !\033[0m\n");
 	else
-		printf("\033[1;31mFAUX !\033[0m\n");
+		ft_printf("\033[1;31mFAUX !\033[0m\n");
 	delete_ac_suff_inf(acs);
 	scmd_clean(&scmd);
 }
 
 void	test_autocomplete()
 {
-	printf(" --- Test autocompletion : ");
+	ft_printf(" --- Test autocompletion : ");
 	//classique
 	test_this_autocomplete("ech", "o", ACS_TYPE_FILE);
 	test_this_autocomplete("dos", "sier", ACS_TYPE_DIR);
@@ -93,16 +93,48 @@ void	test_autocomplete()
 	test_this_autocomplete("\"ech\\\n", "o", ACS_TYPE_FILE);
 }
 
+void	test_this_alias(char *strbase, char *result_needed)
+{
+	t_str_cmd_inf	scmd;
+	t_list			*tk_lst;
+
+	ft_printf("Test alias « %s » :\n", strbase);
+	scmd_init(&scmd, strbase);
+	tk_lst = split_cmd_token(&scmd, alloc.aliastable);
+	ft_printf("(str) Attendu : « %s », resultat : « %s »\n", result_needed, scmd.str);
+	if (ft_strequ(result_needed, scmd.str))
+		ft_printf("\033[1;32mOK !\033[0m\n");
+	else
+		ft_printf("\033[1;31mFAUX !\033[0m\n");
+	ft_lstdel(&tk_lst, del_token);
+	scmd_clean(&scmd);
+}
+
+void	test_alias()
+{
+	ft_printf(" --- Test expand alias : ");
+	test_this_alias("basicalias", "basicresult");
+	test_this_alias("blankalias", "blankresult ");
+	test_this_alias("basicalias basicalias", "basicresult basicalias");
+	test_this_alias("blankalias basicalias", "blankresult  basicresult");
+	test_this_alias("blankalias blankalias", "blankresult  blankresult ");
+	test_this_alias("blankalias basicalias blankalias", "blankresult  basicresult blankalias");
+}
+
 int		main(int argc, char **argv, char **environ)
 {
 	(void)argc;
 	(void)argv;
 
 	setup_alloc(&alloc, argc, argv, environ);
-	//set_alias_if_valid(alloc.aliastable, "ceciestalias", "coucouvaleur", NULL);
-	//set_alias_if_valid(alloc.aliastable, "ceciestautre", "kokokoko", NULL);
-	//set_alias_if_valid(alloc.aliastable, "autretest", "nonpe", NULL);
+	set_alias_if_valid(alloc.aliastable, "aliasrec", "autrealiasrec ; autrealiasrec", NULL);
+	set_alias_if_valid(alloc.aliastable, "autrealiasrec", "aliasrec ; aliasrec", NULL);
+	set_alias_if_valid(alloc.aliastable, "encoreautrealiasrec", "autrealiasrec autrealiasrec", NULL);
+	set_alias_if_valid(alloc.aliastable, "basicalias", "basicresult", NULL);
+	set_alias_if_valid(alloc.aliastable, "blankalias", "blankresult ", NULL);
 	test_autocomplete();
+	ft_printf("\n");
+	test_alias();
 	del_alloc(&alloc);
 	return (0);
 }
