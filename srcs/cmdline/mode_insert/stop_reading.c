6d961593bf3cfca79c9a8c9656548de1d6527e56
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-04-05 10:17:55 +0200

    Petites ameliorations ligne de commande

--- srcs/cmdline/mode_insert/stop_reading.c
+++ srcs/cmdline/mode_insert/stop_reading.c
@@ -6,19 +6,28 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/19 10:15:14 by gguichar          #+#    #+#             */
-/*   Updated: 2019/03/27 01:40:27 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/04/05 10:02:20 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <term.h>
 #include "cmdline.h"
 
 int	handle_cmdline_end(t_cmdline *cmdline)
 {
-	handle_end_key(cmdline);
-	write(STDOUT_FILENO, "\n", 1);
+	static char	*cr_tcap = NULL;
+	static char	*do_tcap = NULL;
+
+	if (cr_tcap == NULL)
+		cr_tcap = tgetstr("cr", NULL);
+	if (do_tcap == NULL)
+		do_tcap = tgetstr("do", NULL);
 	cmdline->input.reading = RSTATE_END;
+	handle_end_key(cmdline);
+	tputs(cr_tcap, 1, t_putchar);
+	tputs(do_tcap, 1, t_putchar);
 	return (1);
 }
 
