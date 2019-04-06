06faa727292cdd4fe37aafb2a472b5efe33bbe0e
Gwennael Guichard <gguichar@student.42.fr>
2019-04-06 20:12:54 +0200

    Fix erreurs dues a une mise a la norme precedente

--- srcs/exec/redirections.c
+++ srcs/exec/redirections.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/21 13:51:13 by gguichar          #+#    #+#             */
-/*   Updated: 2019/04/05 19:45:13 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/04/06 20:06:18 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,38 @@ static int	redirect_file(t_redirect_inf *redirect_inf)
 	int		fd;
 	t_error	error;
 
-	fd = -1;
-	if (redirect_inf->ropt_fd == FD_ERROR)
-		ft_dprintf(STDERR_FILENO, "42sh: -1: bad file descriptor\n");
-	else if (redirect_inf->ropt_fd == FD_NOTSET)
+	is_write = (redirect_inf->red_type == RD_R
+			|| redirect_inf->red_type == RD_RR);
+	fd = open(redirect_inf->ropt_file
+			, is_write ? (O_CREAT | O_WRONLY) : O_RDONLY, 0644);
+	if (fd != -1)
+		close(fd);
+	else
 	{
-		is_write = (redirect_inf->red_type == RD_R
-				|| redirect_inf->red_type == RD_RR);
-		if ((fd = open(redirect_inf->ropt_file
-				, is_write ? (O_CREAT | O_WRONLY) : O_RDONLY, 0644)) == -1)
-		{
-			error = check_file_rights(redirect_inf->ropt_file, FT_FILE
-					, is_write ? W_OK : R_OK);
-			if (is_write && error == ERRC_FILENOTFOUND)
-				error = check_dir_of_file_rights(redirect_inf->ropt_file
-						, X_OK | (is_write ? W_OK : R_OK));
-			ft_dprintf(STDERR_FILENO, "42sh: %s: %s\n", redirect_inf->ropt_file
-					, error_to_str(error));
-		}
+		error = check_file_rights(redirect_inf->ropt_file, FT_FILE
+				, is_write ? W_OK : R_OK);
+		if (is_write && error == ERRC_FILENOTFOUND)
+			error = check_dir_of_file_rights(redirect_inf->ropt_file
+					, X_OK | (is_write ? W_OK : R_OK));
+		ft_dprintf(STDERR_FILENO, "42sh: %s: %s\n", redirect_inf->ropt_file
+				, error_to_str(error));
+		return (0);
 	}
-	return (fd != -1 && close(fd) == 0);
+	return (1);
 }
 
 int			setup_redirection(t_redirect_inf *redirect_inf)
 {
 	if (redirect_inf->red_type != RD_LL)
-		return (redirect_file(redirect_inf));
+	{
+		if (redirect_inf->ropt_fd == FD_NOTSET)
+			return (redirect_file(redirect_inf));
+		else if (redirect_inf->ropt_fd == FD_ERROR)
+		{
+			ft_dprintf(STDERR_FILENO, "42sh: -1: bad file descriptor\n");
+			return (0);
+		}
+	}
 	return (1);
 }
 
