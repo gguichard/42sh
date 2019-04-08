f12d05be0eb51642d00516402bed6d524b6b0a5d
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-04-08 10:21:46 +0200

    Recreation du ft_splitwhitespace en plus opti

--- srcs/expand/ft_splitwhitespace.c
+++ srcs/expand/ft_splitwhitespace.c
@@ -1,96 +1,58 @@
-#include "shell.h"
+#include <stdlib.h>
+#include "libft.h"
 #include "expand.h"
 
-static unsigned int	ft_word_counter(char const *s)
+static size_t	ft_word_len(char const *s)
 {
-	int				i;
-	unsigned int	nb_word;
+	size_t	len;
 
-	nb_word = 0;
-	i = 0;
-	if (s[0] && ft_isspace(s[0]) == 0)
-		nb_word++;
-	while (s[i])
-	{
-		if (i > 0 && ft_isspace(s[i]) == 0 && ft_isspace(s[i - 1]))
-			nb_word++;
-		i++;
-	}
-	return (nb_word);
+	len = 0;
+	while (s[len] != '\0' && !ft_isspace(s[len]))
+		len++;
+	return (len);
 }
 
-static int			ft_position_word(char const *s, unsigned int word_n)
+static size_t	ft_count_words(char const *s)
 {
-	int				i;
-	unsigned int	i_word;
+	size_t	words;
 
-	i = 0;
-	i_word = 0;
-	while (s[i] && ft_isspace(s[i]))
-		i++;
-	while (s[i])
+	words = 0;
+	while (*s != '\0')
 	{
-		if (ft_isspace(s[i]) == 0 && i_word == word_n)
-			return (i);
-		if (i > 0 && ft_isspace(s[i]) && ft_isspace(s[i + 1]) == 0)
-			i_word++;
-		i++;
+		if (ft_isspace(*s))
+			s++;
+		else
+		{
+			words++;
+			s += ft_word_len(s);
+		}
 	}
-	return (i);
+	return (words);
 }
 
-static int			ft_counter_letter(char const *s, unsigned int word_n)
+char			**ft_splitwhitespace(const char *s)
 {
-	int	pos_word;
-	int	nb_letter;
+	size_t	words;
+	char	**tab;
+	size_t	i;
+	size_t	word_len;
 
-	nb_letter = 0;
-	pos_word = ft_position_word(s, word_n);
-	while (s[pos_word] && ft_isspace(s[pos_word]) == 0)
-	{
-		pos_word++;
-		nb_letter++;
-	}
-	return (nb_letter);
-}
-
-static void			ft_fill(char const *s, unsigned int wrd_n, char **split)
-{
-	int	i;
-	int	j;
-
-	i = ft_position_word(s, wrd_n);
-	j = 0;
-	while (s[i] && ft_isspace(s[i]) == 0)
-	{
-		split[wrd_n][j] = s[i];
-		i++;
-		j++;
-	}
-	split[wrd_n][j] = '\0';
-}
-
-char				**ft_splitwhitespace(char const *s)
-{
-	unsigned int	nb_word;
-	unsigned int	i;
-	char			**split;
-	int				nb_letter;
-
-	i = 0;
-	if (!s)
-		return (0);
-	nb_word = ft_word_counter(s);
-	if (!(split = (char**)malloc(sizeof(char*) * (nb_word + 1))))
+	if (s == NULL)
 		return (NULL);
-	while (i < nb_word)
+	words = ft_count_words(s);
+	if (!(tab = (char **)malloc(sizeof(*tab) * (words + 1))))
+		return (NULL);
+	tab[words] = 0;
+	i = 0;
+	while (i < words)
 	{
-		nb_letter = ft_counter_letter(s, i);
-		if (!(split[i] = (char*)malloc(sizeof(char) * (nb_letter + 1))))
-			return (NULL);
-		ft_fill((char const *)s, i, split);
+		while (ft_isspace(*s))
+			s++;
+		word_len = ft_word_len(s);
+		if (!(tab[i] = ft_strsub(s, 0, word_len)))
+			return (ft_strtab_free(tab));
+		s += word_len;
 		i++;
 	}
-	split[nb_word] = 0;
-	return (split);
+	return (tab);
 }
