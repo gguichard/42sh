3482c0882bdacaa3d71b18b7a73020f5e722f87e
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-04-08 14:27:50 +0200

    Utilisation de remove_only_escaped_newline

--- srcs/inhib/inhibitor_remove.c
+++ srcs/inhib/inhibitor_remove.c
@@ -6,7 +6,7 @@
 /*   By: tcollard <tcollard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 17:44:01 by tcollard          #+#    #+#             */
-/*   Updated: 2019/04/08 14:25:20 by tcollard         ###   ########.fr       */
+/*   Updated: 2019/04/08 14:26:33 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 #include "inhibitor.h"
 #include "str_cmd_inf.h"
 
-void	remove_escaped_char(t_str_cmd_inf *str_cmd, char **input, size_t *pos,
-		int next)
+void	remove_escaped_char(t_str_cmd_inf *str_cmd, char **input, size_t *pos
+		, int next)
 {
 	size_t	len;
 
@@ -27,14 +27,14 @@ void	remove_escaped_char(t_str_cmd_inf *str_cmd, char **input, size_t *pos,
 		*pos = 1;
 	len = ft_strlen(*input + *pos);
 	ft_memmove((void*)(*input + *pos - 1),
-		(const void*)(*input + *pos), len);
+			(const void*)(*input + *pos), len);
 	(*input)[*pos + len - 1] = '\0';
 	if (scmd_cur_char_is_escaped(str_cmd) == 1
 			&& scmd_cur_char(str_cmd) == '\n' && !str_cmd->is_in_quote)
 	{
 		len = ft_strlen(*input + *pos);
 		ft_memmove((void*)(*input + *pos - 1),
-			(const void*)(*input + *pos), len);
+				(const void*)(*input + *pos), len);
 		(*input)[*pos + len - 1] = '\0';
 		if (*pos > 0)
 			*pos -= 1;
@@ -56,8 +56,8 @@ void	remove_last_char(t_str_cmd_inf *str_cmd, size_t *pos, char **input)
 		*pos -= 1;
 	}
 	else if ((scmd_cur_char(str_cmd) == '\'' && (*input)[*pos] == '\''
-			&& str_cmd->is_in_quote) || (scmd_cur_char(str_cmd) == '"'
-			&& (*input)[*pos] == '"' && str_cmd->is_in_dbquote))
+				&& str_cmd->is_in_quote) || (scmd_cur_char(str_cmd) == '"'
+					&& (*input)[*pos] == '"' && str_cmd->is_in_dbquote))
 	{
 		*pos += 1;
 		remove_escaped_char(str_cmd, input, pos, 1);
@@ -66,12 +66,12 @@ void	remove_last_char(t_str_cmd_inf *str_cmd, size_t *pos, char **input)
 	str_cmd->pos += 1;
 }
 
-void	remove_last_quote(t_str_cmd_inf *str_cmd, char **array, size_t *pos,
-		size_t index)
+void	remove_last_quote(t_str_cmd_inf *str_cmd, char **array, size_t *pos
+		, size_t index)
 {
 	str_cmd->pos -= 1;
 	if ((scmd_cur_char(str_cmd) == '\"'
-			&& !scmd_cur_char_is_escaped(str_cmd))
+				&& !scmd_cur_char_is_escaped(str_cmd))
 			|| scmd_cur_char(str_cmd) == '\\')
 	{
 		str_cmd->pos += 1;
@@ -85,7 +85,7 @@ void	remove_last_quote(t_str_cmd_inf *str_cmd, char **array, size_t *pos,
 	}
 }
 
-int		remove_only_escaped_backslash_n(char **str)
+int		remove_only_escaped_newline(char **str)
 {
 	t_str_cmd_inf	str_cmd;
 	size_t			pos;
@@ -97,13 +97,13 @@ int		remove_only_escaped_backslash_n(char **str)
 	{
 		if (str_cmd.is_in_quote || str_cmd.is_in_dbquote)
 			while (scmd_cur_char(&str_cmd) && (str_cmd.is_in_quote
-					|| str_cmd.is_in_dbquote))
+						|| str_cmd.is_in_dbquote))
 				pos += scmd_move_to_next_char(&str_cmd);
 		else if (scmd_cur_char(&str_cmd) == '\n'
 				&& scmd_cur_char_is_escaped(&str_cmd))
 		{
 			ft_memmove((void *)(*str + pos - 1), (void *)(*str + pos + 1)
-				, ft_strlen(*str + pos));
+					, ft_strlen(*str + pos));
 			(*str)[pos + ft_strlen(*str + pos)] = '\0';
 			scmd_move_to_next_char(&str_cmd);
 		}
