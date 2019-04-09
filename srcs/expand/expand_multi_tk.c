a08ec8c8b41236a5b7b3e06241aeffe6bfe557f1
tcollard <tcollard@student.42.fr>
2019-04-09 19:32:36 +0200

    Home expand update

--- srcs/expand/expand_multi_tk.c
+++ srcs/expand/expand_multi_tk.c
@@ -6,7 +6,7 @@
 /*   By: tcollard <tcollard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 23:43:30 by tcollard          #+#    #+#             */
-/*   Updated: 2019/04/09 18:46:56 by tcollard         ###   ########.fr       */
+/*   Updated: 2019/04/09 19:30:03 by tcollard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,6 @@ int		expand_var_to_tab(char ***array, size_t len, size_t *pos_array
 		return (1);
 	if (!(replace = ft_memalloc(sizeof(char*) * (len + 1))))
 		return (0);
-	replace[len] = NULL;
 	replace_in_tab(&i, &replace, *array);
 	sub = ft_strsub((*array)[get_pos_in_array(*array)], 0, save);
 	replace[i++] = ft_strjoin(sub, new_array[0]);
